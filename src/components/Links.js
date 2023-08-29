2b7df3fad9c7dde5a8f11dc8a10cd0c5988ef1d2
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-08-29 23:47:40 +0300

    complete assignment

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function Links({ github, linkedin }) {
   return (
     <div>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,17 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map((project, index) => (
+          <ProjectItem
+            key={index}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}</div>
     </div>
   );
 }
 
 export default ProjectList;
+
